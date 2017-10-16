967c51c5e9b84f78142bc1aa44996b72bc10a72c
huayu <faint77@hotmail.com>
2017-10-16 15:55:46 +0800

    viewer init

--- imports/ui/Viewer.jsx
+++ imports/ui/Viewer.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { Button, Navbar, NavItem, Nav, NavDropdown, MenuItem, Jumbotron } from 'react-bootstrap';
+import { Meteor } from 'meteor/meteor';
+import dicomParse from 'dicom-parser';
+import FS from 'fs'
+import cornerstone from 'cornerstone-core'
 
 const style = {
   top: {
@@ -32,34 +37,34 @@ const style = {
   },
   patientInfo: {
     position: 'absolute',
-    top:'0px',
-    left:'0px',
-    height:'100px',
-    width:'100px',
+    top: '0px',
+    left: '0px',
+    height: '100px',
+    width: '100px',
     backgroundColor: 'black'
   },
   dicomInfo: {
     position: 'absolute',
-    top:'0px',
-    right:'0px',
-    height:'100px',
-    width:'100px',
+    top: '0px',
+    right: '0px',
+    height: '100px',
+    width: '100px',
     backgroundColor: 'orange'
   },
   number: {
     position: 'absolute',
-    bottom:'0px',
-    left:'0px',
-    height:'100px',
-    width:'100px',
+    bottom: '0px',
+    left: '0px',
+    height: '100px',
+    width: '100px',
     backgroundColor: 'black'
   },
   timeInfo: {
     position: 'absolute',
-    bottom:'0px',
-    right:'0px',
-    height:'100px',
-    width:'100px',
+    bottom: '0px',
+    right: '0px',
+    height: '100px',
+    width: '100px',
     backgroundColor: 'orange'
   }
 
@@ -68,11 +73,52 @@ const style = {
 export default class Viewer extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      container: {},
+      dicomObj: {}
+    }
+    // console.log(this.state.dicomObj)
+    this.setSlice = this.setSlice.bind(this);
   }
   componentDidMount() {
+    cornerstone.enable(ReactDOM.findDOMNode(this.refs.viewerContainer))
+    this.setState({
+      container: ReactDOM.findDOMNode(this.refs.viewerContainer)
+    });
+    let that = this;
+    Meteor.call('prepareDicoms', this.props.location.query.caseId, (error) => {
+      if (error) {
+        console.log(error)
+      } else {
+        that.setSlice(1)
+      }
+    })
   }
 
+  setSlice(index) {
+    // console.log(this.state.dicomObj)
+    if (!this.state.dicomObj[index]) {
+      Meteor.call('getDicom', index, (err, result) => {
+        let image = result;
+        // console.log(image)
 
+        let pixelDataElement = image.pixelData;
+        let pixelData = new Uint16Array(image.imageBuf.buffer, image.pixelDataOffset, image.pixelDataLength / 2);
+        image.getPixelData = function(){
+          return pixelData
+        }
+        let currentObj = this.state.dicomObj
+        currentObj[index] = image
+        this.setState({
+          dicomObj: currentObj
+        })
+        console.log(this.state.dicomObj)
+        cornerstone.displayImage(this.state.container, this.state.dicomObj[index])
+      })
+    } else {
+      cornerstone.displayImage(this.state.container, this.state.dicomObj[index])
+    }
+  }
 
   render() {
     return (
@@ -86,14 +132,14 @@ export default class Viewer extends Component {
                 <NavItem eventKey={3} href="#">zoom/pan</NavItem>
                 <NavItem eventKey={4} href="#">draw</NavItem>
               </Nav>
-
             </Navbar.Collapse>
           </Navbar>
         </div>
         <div style={style.container} className="container">
-          <div className="col-xs-3 left-container"></div>
+          <div className="col-xs-3 left-container">
+          </div>
           <div className="col-xs-6 center-container">
-            <div style={style.viewer} id="viewer" className="viewer" >
+            <div style={style.viewer} ref="viewerContainer" id="viewer" className="viewer" >
               <div style={style.patientInfo} id="patientInfo"></div>
               <div style={style.dicomInfo} id="dicomInfo"></div>
               <div style={style.number} id="number"></div>
---
+++ imports/ui/viewer/ImageRender.js
@@ -0,0 +1,133 @@
+/**
+ * author: yunzhe
+ * This class is used to deal with image rendering actions,
+ * including changing slices, window width/center, size, position.
+ */
+class ImageRender {
+
+  /**
+   * constructor
+   * @param elementId the div id which to render image
+   * @param numberOfImage number of images of a seris
+   * @param dicomProperty
+   */
+  constructor(elementId, numberOfImage, dicomProperty) {
+    this.element = $("#" + elementId).get(0);
+    cornerstone.enable(this.element);
+    this.numberOfImage = numberOfImage;
+    this.index = 1;
+    this.imageLoader = new ImageLoader(cornerstone);
+    this.imageData = new ImageData(numberOfImage);
+    this.image = {
+      imageId: 0,
+      minPixelValue: dicomProperty.minPixelValue,
+      maxPixelValue: dicomProperty.maxPixelValue,
+      slope: dicomProperty.slope,
+      intercept: dicomProperty.intercept,
+      windowCenter: dicomProperty.windowCenter,
+      windowWidth: dicomProperty.windowWidth,
+      getPixelData: function () {},
+      columns: dicomProperty.columns,
+      rows: dicomProperty.rows,
+      width: dicomProperty.width,
+      height: dicomProperty.height,
+      sizeInBytes: dicomProperty.sizeInBytes,
+      render: cornerstone.renderGrayscaleImage
+    };
+  }
+
+
+  /**
+   * increase slice number
+   */
+  increaseSlice() {
+    if (this.index < this.numberOfImage) {
+      this.index++;
+      this.setSlice(this.index);
+    }
+  }
+
+  /**
+   * decrease slice number
+   */
+  decreaseSlice() {
+    if (this.index > 1) {
+      this.index--;
+      this.setSlice(this.index);
+    }
+  }
+
+  /**
+   * set slice number to index
+   * @param index the index jump to
+   */
+  setSlice(index) {
+    this.imageData.getSingleImage(index, this.element, this.renderImage, this);
+  }
+
+  /**
+   * render image, used as callback
+   * @param element the div element to render
+   * @param pixel image pixel
+   * @param index to specify different imageId
+   * @param self this object
+   */
+  renderImage(element, pixel, index, self) {
+    self.image.imageId = index;
+    self.image.getPixelData = function () {
+      return pixel;
+    };
+    cornerstone.displayImage(element, self.image);
+  }
+
+
+
+  /**
+   * set window center
+   * @param windowCenter
+   */
+  setWindowCenter(windowCenter) {
+    let viewport = cornerstone.getViewport(this.element);
+    viewport.voi.windowCenter = windowCenter;
+    cornerstone.setViewport(element, viewport);
+  }
+
+  /**
+   * set window width
+   * @param windowWidth
+   */
+  setWindowWidth(windowWidth) {
+    let viewport = cornerstone.getViewport(this.element);
+    viewport.voi.windowWidth = windowWidth;
+    cornerstone.setViewport(element, viewport);
+  }
+
+  /**
+   * set image size
+   * @param scale
+   */
+  setImageScale(scale) {
+    let viewport = cornerstone.getViewport(this.element);
+    viewport.scale = scale;
+    cornerstone.set(element, viewport);
+  }
+
+  /**
+   * set image position
+   * @param x
+   * @param y
+   */
+  setImagePosition(x, y) {
+    let viewport = cornerstone.get(this.element);
+    viewport.translation.x = x;
+    viewport.translation.y = y;
+    cornerstone.setViewport(this.element, viewport);
+  }
+
+  /**
+   * reset to default viewport settings
+   */
+  reset() {
+    cornerstone.reset(this.element);
+  }
+}
