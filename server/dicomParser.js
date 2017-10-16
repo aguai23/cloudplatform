418f88b3d8c0112db42967df59581c534c3252f8
chengyu <cuichengyu@pvmedtech.com>
2017-10-16 18:33:26 +0800

    add multi-user support

--- server/dicomParser.js
+++ server/dicomParser.js
@@ -17,10 +17,13 @@ Meteor.methods({
   /**
    * Parse all DICOMs that belongs to the requested case
      @param caseId the id of requested caseId
-     @returns {status: FAILURE/SUCCESS}
+     @returns {status}
    */
   prepareDicoms(caseId) {
-    var result = {status: 'FAILURE'};
+    var result = {status: 'FAILURE'},
+        userId = Meteor.userId();
+
+    dicomObj[userId] = {};
 
     /**
      * get requested case
@@ -30,20 +33,19 @@ Meteor.methods({
     /**
      * parse all DICOMs and save
      */
-    if(foundCase.files && foundCase.files.length > 0) {
+    if(userId && foundCase.files && foundCase.files.length > 0) {
       for(var i = 0; i < foundCase.files.length; i++) {
         var data = fs.readFileSync(foundCase.files[i]);
         var dataset = dicomParser.parseDicom(data);
         var index = parseInt(dataset.string('x00200013'));
 
-        dicomObj[index-1] = dataset;
+        dicomObj[userId][index-1] = dataset;
       }
 
       result.status = "SUCCESS";
     }
 
     return result;
-
   },
 
   /**
@@ -52,26 +54,28 @@ Meteor.methods({
    * @return {Object} contains the status and parsed information for the DICOM
    */
   getDicom(index) {
-    if(!dicomObj[index-1])  return {status: 'FAILURE'};
+    var userId = Meteor.userId();
+
+    if(!userId || !dicomObj[userId][index-1])  return {status: 'FAILURE'};
 
     var result = {};
 
     result.status = 'SUCCESS';
 
-    result.imageBuf = dicomObj[index-1].byteArray;
-    result.pixelDataOffset = dicomObj[index-1].elements.x7fe00010.dataOffset;
-    result.pixelDataLength = dicomObj[index-1].elements.x7fe00010.length;
+    result.imageBuf = dicomObj[userId][index-1].byteArray;
+    result.pixelDataOffset = dicomObj[userId][index-1].elements.x7fe00010.dataOffset;
+    result.pixelDataLength = dicomObj[userId][index-1].elements.x7fe00010.length;
 
     result.getPixelData = function() {};
 
     result.minPixelValue = 0;
     result.maxPixelValue = 4096;
-    result.slope = dicomObj[index-1].string('x00281053').length === 0 ? 0 : parseInt(dicomObj[index-1].string('x00281053'));
-    result.intercept = dicomObj[index-1].string('x00281052').length === 0 ? -1024 : parseInt(dicomObj[index-1].string('x00281052'));
+    result.slope = dicomObj[userId][index-1].string('x00281053').length === 0 ? 0 : parseInt(dicomObj[userId][index-1].string('x00281053'));
+    result.intercept = dicomObj[userId][index-1].string('x00281052').length === 0 ? -1024 : parseInt(dicomObj[userId][index-1].string('x00281052'));
     result.windowCenter = -600;
     result.windowWidth = 1500;
-    result.columns = dicomObj[index-1].string('x00280011').length === 0 ? 512 : parseInt(dicomObj[index-1].string('x00280011'));
-    result.rows = dicomObj[index-1].string('x00280010').length === 0 ? 512 : parseInt(dicomObj[index-1].string('x00280010'));
+    result.columns = dicomObj[userId][index-1].string('x00280011').length === 0 ? 512 : parseInt(dicomObj[userId][index-1].string('x00280011'));
+    result.rows = dicomObj[userId][index-1].string('x00280010').length === 0 ? 512 : parseInt(dicomObj[userId][index-1].string('x00280010'));
     result.width = result.columns;
     result.height = result.rows;
     result.sizeInBytes = result.rows * result.columns * 2;
