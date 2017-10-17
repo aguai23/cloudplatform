0a102d7208169da0cd4109bfae1015f1c47d55f3
chengyu <cuichengyu@pvmedtech.com>
2017-10-17 14:45:44 +0800

    adjust css styles of viewer

--- imports/ui/Viewer.jsx
+++ imports/ui/Viewer.jsx
@@ -1,39 +1,47 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Navbar, NavItem, Nav, NavDropdown, MenuItem, Jumbotron } from 'react-bootstrap';
+import { Button, Col, Navbar, NavItem, Nav, NavDropdown, MenuItem, Jumbotron } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import dicomParse from 'dicom-parser';
-import FS from 'fs'
-import cornerstone from 'cornerstone-core'
-import cornerstoneTools from 'cornerstone-tools'
+import FS from 'fs';
+import cornerstone from 'cornerstone-core';
+import cornerstoneTools from 'cornerstone-tools';
+import FontAwesome from 'react-fontawesome';
+
 const style = {
+  body: {
+    backgroundColor: 'black',
+    minHeight: '100%'
+  },
   top: {
-    height: '50px',
+    height: 'auto',
     backgroundColor: '#7f7f7f',
     width: '100%',
     position: 'relative',
     left: '0px',
   },
   bottom: {
-    position: 'relative',
+    position: 'fixed',
     width: '100%',
-    bottom: '0px',
-    left: '0px',
-    height: '100px',
+    left: '0',
+    bottom: '0',
+    height: '0',
     backgroundColor: '#7f7f7f'
   },
   container: {
-    // top: '100px',
-    height: '70%',
     position: 'relative',
     width: '100%',
     left: '0px',
+    border: '1px solid #42f4ee'
   },
   viewer: {
-    height: '512px',
-    width: '512px',
+    top: '30px',
+    height: '800px',
+    width: '100%',
     position: 'relative',
-    backgroundColor: 'green'
+    backgroundColor: 'green',
+    position: 'relative',
+    margin: '0 auto'
   },
   patientInfo: {
     position: 'absolute',
@@ -82,8 +90,8 @@ export default class Viewer extends Component {
         dicomObj: {},
         index:1,
         imageNumber:0
-
     };
+
     this.setSlice = this.setSlice.bind(this);
     this.increaseSlice = this.increaseSlice.bind(this);
     this.decreaseSlice = this.decreaseSlice.bind(this);
@@ -97,6 +105,13 @@ export default class Viewer extends Component {
      * will run after elements rendered
      */
   componentDidMount() {
+    /**
+     * set the dynamic height for container
+     */
+    this.setState({
+      containerHeight: (window.innerHeight - document.getElementById('top').clientHeight) + 'px'
+    });
+
     cornerstone.enable(document.getElementById("viewer"));
     this.setState({
       container: ReactDOM.findDOMNode(this.refs.viewerContainer)
@@ -236,34 +251,49 @@ export default class Viewer extends Component {
 
   render() {
     return (
-      <div id="body">
-        <div style={style.top}>
-          <Navbar inverse collapseOnSelect>
+      <div id="body" style={style.body}>
+        <div id="top" style={style.top}>
+          <Navbar inverse collapseOnSelect style={{marginBottom: '0'}}>
             <Navbar.Collapse>
               <Nav>
-                <NavItem eventKey={1} href="#" onClick={this.setScrollTool}>scroll</NavItem>
-                <NavItem eventKey={2} href="#" onClick={this.setWindowTool}>wl/wc</NavItem>
-                <NavItem eventKey={3} href="#" onClick={this.setZoomTool}>zoom/pan</NavItem>
-                <NavItem eventKey={4} href="#" onClick={this.setDrawTool}>draw</NavItem>
+                <NavItem eventKey={1} href="#" onClick={this.setScrollTool}>
+                  <div>
+                    <FontAwesome name='gear' size='2x'/>
+                  </div>
+                  <span>scroll</span>
+                </NavItem>
+                <NavItem eventKey={2} href="#" onClick={this.setWindowTool}>
+                  <div>
+                    <FontAwesome name='adjust' size='2x'/>
+                  </div>
+                  <span>wl/wc</span>
+                </NavItem>
+                <NavItem eventKey={3} href="#" onClick={this.setZoomTool}>
+                  <div>
+                    <FontAwesome name='search' size='2x'/>
+                  </div>
+                  <span>zoom/pan</span>
+                </NavItem>
+                <NavItem eventKey={4} href="#" onClick={this.setDrawTool}>
+                  <div>
+                    <FontAwesome name='square-o' size='2x'/>
+                  </div>
+                  <span>draw</span>
+                </NavItem>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
         </div>
-        <div style={style.container} className="container">
-          <div className="col-xs-3 left-container">
+        <div style={{...style.container, ...{height: this.state.containerHeight}}} className="container">
+          <div style={style.viewer} ref="viewerContainer" id="viewer" >
+            <div style={style.patientInfo} id="patientInfo"></div>
+            <div style={style.dicomInfo} id="dicomInfo"></div>
+            <div style={style.number} id="number"></div>
+            <div style={style.timeInfo} id="timeInfo"></div>
           </div>
-          <div className="col-xs-6 center-container">
-            <div style={style.viewer} ref="viewerContainer" id="viewer" className="viewer" >
-              <div style={style.patientInfo} id="patientInfo"></div>
-              <div style={style.dicomInfo} id="dicomInfo"></div>
-              <div style={style.number} id="number"></div>
-              <div style={style.timeInfo} id="timeInfo"></div>
-            </div>
-          </div>
-          <div className="col-xs-3 right-container"></div>
         </div>
 
-        <div style={style.bottom} className="bottom"></div>
+        <div style={style.bottom}></div>
       </div>
     )
   }
