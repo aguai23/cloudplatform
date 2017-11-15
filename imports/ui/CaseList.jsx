fdbaab9c8750b1c4964babfc9891e290f5a2bc66
huayu <faint77@hotmail.com>
2017-11-15 20:21:35 +0800

    rebuild caseList

--- imports/ui/CaseList.jsx
+++ imports/ui/CaseList.jsx
@@ -1,39 +1,13 @@
 import React, { Component } from 'react';
-import { Navbar, Table, Button } from 'react-bootstrap';
+import { Navbar, Table, Button, Form, FormControl, FormGroup, ControlLabel, Nav, NavItem, Col, InputGroup, DropdownButton, MenuItem } from 'react-bootstrap';
 import { Cases } from '../api/cases';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { browserHistory, Link } from 'react-router';
 import { toast, ToastContainer } from 'react-toastify';
+import DatasetMenu from './DatasetMenu.jsx';
 import "./css/caseList.css";
-const style = {
-  searchResult: {
-    marginTop: '10px',
-    marginBottom: '20px',
-    paddingLeft: '20px',
-    fontSize: '18px'
-  },
-  searchBar: {
-    width: "17%",
-    height: "50%",
-    marginLeft: "10px",
-    float: "left",
-    borderStyle: "solid",
-    borderColor: "darkcyan",
-    backgroundColor: "aliceblue"
-  },
-  caseList: {
-    marginRight: "10px",
-    width: "80%",
-    marginTop: "10px",
-    float: "right"
-  },
-  formElement: {
-    marginTop: "20px",
-    marginBottom: "30px",
-    marginLeft: "20px"
-  }
-};
+import './css/common/eightCols.css';
 
 export class CaseList extends Component {
   constructor(props) {
@@ -45,7 +19,7 @@ export class CaseList extends Component {
       patientID: "",
       patientName: "",
       patientAge: 0,
-      patientSex: "M",
+      patientSex: 'M',
       modality: "CT",
       startTime: "",
       endTime: ""
@@ -53,12 +27,21 @@ export class CaseList extends Component {
 
     this.searchCase = this.searchCase.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleModalitySelect = this.handleModalitySelect.bind(this);
+    this.handleSexSelect = this.handleSexSelect.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.cases !== this.state.cases) {
       this.setState({ cases: nextProps.cases });
     }
+  }
+
+  componentDidMount() {
+    this.setState({
+      containerHeight: window.innerHeight - document.getElementById('header').clientHeight,
+      // bottomDivHeight: window.innerHeight - document.getElementById('header').clientHeight - document.getElementById('searchBar').clientHeight - 120
+    });
   }
 
   /**
@@ -166,6 +149,37 @@ export class CaseList extends Component {
     });
   }
 
+  handleSexSelect(eventKey){
+    this.setState({
+      patientSex: eventKey
+    })
+  }
+
+  handleModalitySelect(eventKey){
+    this.setState({
+      modality: eventKey
+    })
+  }
+
+  onTabSelectHandler(eventKey) {
+      event.preventDefault();
+      console.log(eventKey)
+      return
+  
+      if (this.state.tabActiveKey === eventKey) return;
+  
+      this.setState({ tabActiveKey: eventKey });
+  
+      if (eventKey === 'PUB') {
+        browserHistory.replace('')
+      } else if (eventKey === 'PVT') {
+        this.setState({ dataCollections: this.props.privateDataCollections });
+      } else if (eventKey === 'FAV') {
+        this.setState({ dataCollections: this.props.favoriteDataCollections });
+      }
+    
+  }
+
   render() {
     const that = this;
     let tHead = this.state.cases.length > 0 ?
@@ -188,112 +202,137 @@ export class CaseList extends Component {
 
     return (
       <div>
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <button onClick={browserHistory.goBack} style={{ border: "none", background: "transparent" }}>
-                <i className={"fa fa-angle-left"} style={{ color: "red", fontSize: "20px" }}>返回</i>
-              </button>
-              <Link to={`/newCase?collection=${this.props.params.collectionName}`}>
-                <i className="fa fa-plus" style={{ color: "green", fontSize: "20px", marginLeft: "20px" }}>新建</i>
-              </Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-        </Navbar>
-        <div className={"searchBar"} style={style.searchBar}>
-          <div style={style.formElement}>
-            检查号：
-            <input type={"text"} name={"patientID"} value={this.state.patientID} onChange={this.handleInputChange} />
+        <div id="modal-base"/>
+        <div className="eight-cols" style={{ height: this.state.containerHeight }}>
+          <div className="col-sm-1 nav-container">
+            <DatasetMenu activeKey={this.state.tabActiveKey} selectHandler={this.onTabSelectHandler} />
           </div>
-          <div style={style.formElement}>
-            姓名：
-            <input type={"text"} name={"patientName"} value={this.state.patientName} onChange={this.handleInputChange} />
-          </div>
-          <div style={style.formElement}>
-            年龄：
-            <input type={"number"} name={"patientAge"} value={this.state.patientAge} onChange={this.handleInputChange} />
-          </div>
-          <div style={style.formElement}>
-            性别：
-            <select name={"patientSex"} value={this.state.patientSex} onChange={this.handleInputChange}>
-              <option value={"M"}>男</option>
-              <option value={"F"}>女</option>
-            </select>
-          </div>
-          <div style={style.formElement} >
-            设备：
-            <select name={"modality"} value={this.state.modality} onChange={this.handleInputChange}>
-              <option value={"CT"}>CT</option>
-              <option value={"MRI"}>MRI</option>
-              <option value={"DSA"}>DSA</option>
-              <option value={"DR"}>DR</option>
-              <option value={"CR"}>CR</option>
-              <option value={"RF"}>RF</option>
-              <option value={"MG"}>MG</option>
-              <option value={"US"}>US</option>
-            </select>
-          </div>
-          <div style={style.formElement}>
-            起始时间：
-            <input type={"date"} name={"startTime"} value={this.state.startTime} onChange={this.handleInputChange} />
-          </div>
-          <div style={style.formElement}>
-            终止时间：
-            <input type={"date"} name={"endTime"} value={this.state.endTime} onChange={this.handleInputChange} />
-          </div>
-          <div style={style.formElement}>
-            <Button bsStyle={"primary"} onClick={this.searchCase}>查询</Button>
-            <Button bsStyle={"danger"} onClick={() => this.reset()} style={{ marginLeft: "20px" }}>重置</Button>
-          </div>
-        </div>
+          <div className="col-sm-7 content-container">
+            <div className="row" style={{ marginTop: '30px' }}>
+              <div className="col-md-2 col-md-offset-10">
+                <button onClick={browserHistory.goBack} style={{ border: "none", background: "transparent" }}>
+                  <i className={"fa fa-angle-left"} style={{ color: '#255BA8', fontSize: "15px" }}>返回</i>
+                </button>
+                <Link to={`/newCase?collection=${this.props.params.collectionName}`}>
+                  <i className="fa fa-plus" style={{ color: '#255BA8', fontSize: "15px", marginLeft: "20px" }}>新建</i>
+                </Link>
+              </div>
+            </div>
+            <div id="searchBar" style={{ height: '80px', marginTop: '5px' }}>
+              <div className="row">
+                <div className="col-md-5">
+                  <div className="col-md-4" style={{ paddingLeft: 0 }}>
+                    <FormGroup>
+                      <InputGroup>
+                        <InputGroup.Addon>检查号</InputGroup.Addon>
+                        <FormControl name="patientID" type="text" onChange={this.handleInputChange} />
+                      </InputGroup>
+                    </FormGroup>
+                  </div>
+                  <div className="col-md-4" style={{ paddingLeft: 0 }}>
+                    <FormGroup>
+                      <InputGroup>
+                        <InputGroup.Addon>姓名</InputGroup.Addon>
+                        <FormControl name="patientName" type="text" onChange={this.handleInputChange} />
+                      </InputGroup>
+                    </FormGroup>
+                  </div>
+                  <div className="col-md-4" style={{ paddingLeft: 0, paddingRight: 0 }}>
+                    <FormGroup>
+                      <InputGroup>
+                        <InputGroup.Addon>年龄</InputGroup.Addon>
+                        <FormControl name="patientAge" type="text" onChange={this.handleInputChange} />
+                      </InputGroup>
+                    </FormGroup>
+                  </div>
+                </div>
+                <div className="col-md-4" style={{ paddingLeft: 0 }}>
+                  <div className="col-md-7" style={{ paddingLeft: 0 }}>
+                    <FormGroup>
+                      <InputGroup>
+                        <InputGroup.Addon>时间</InputGroup.Addon>
+                        <FormControl type='date' name="startTime" value={this.state.startTime} onChange={this.handleInputChange} />
+                      </InputGroup>
+                    </FormGroup>
+                  </div>
+                  <div className="col-md-5" style={{ paddingLeft: 0, paddingRight: 0 }}>
+                    <FormGroup>
+                      <FormControl type='date' name="endTime" value={this.state.endTime} onChange={this.handleInputChange} />
 
-        <div className="container" style={style.caseList}>
-          <div style={style.searchResult}>{searchResult}</div>
-          <Table striped bordered condensed hover>
-            <thead>
-              {tHead}
-            </thead>
-            <tbody>
-              {this.state.cases.length > 0 && this.state.cases.map((specificCase) => {
-                return (
-                  <tr key={specificCase._id} onDoubleClick={() => CaseList.jumpTo(specificCase._id)}>
-                    <td>{specificCase.accessionNumber}</td>
-                    <td>{specificCase.patientID}</td>
-                    <td>{specificCase.patientName}</td>
-                    <td>{specificCase.patientAge}</td>
-                    <td>{specificCase.patientSex === 'M' ? '男' : '女'}</td>
-                    <td>{specificCase.studyID}</td>
-                    <td>{specificCase.modality}</td>
-                    <td>{specificCase.studyDate}</td>
-                    <td>{specificCase.patientBirthDate}</td>
-                    <td>{specificCase.studyDescription}</td>
-                    <td>
-                      <Link to={{
-                        pathname: '/viewer',
-                        state: { caseId: specificCase._id }
-                      }} className="glyphicon glyphicon-picture" />
-                      &nbsp;&nbsp;&nbsp;
-                    <Link to={`/newCase?id=${specificCase._id}`} className="glyphicon glyphicon-pencil" />
-                      &nbsp;&nbsp;&nbsp;
-                    <span className="glyphicon glyphicon-trash" onClick={that.deleteCase.bind(this, specificCase._id)} />
-                    </td>
-                  </tr>
-                )
-              })
-              }
-            </tbody>
-          </Table>
+                    </FormGroup>
+                  </div>
+                </div>
+                <div className="col-md-3" style={{ paddingLeft: 0 }}>
+                  <DropdownButton id="patientSex" onSelect={this.handleSexSelect} title={this.state.patientSex ? this.state.patientSex === 'M' ? '男' : '女' : "性别"}>
+                    <MenuItem eventKey="M">男</MenuItem>
+                    <MenuItem eventKey="F">女</MenuItem>
+                  </DropdownButton>
+                  <DropdownButton id="modality" onSelect={this.handleModalitySelect} title={this.state.modality} style={{ marginLeft: '3px' }}>
+                    <MenuItem eventKey="CT">CT</MenuItem>
+                    <MenuItem eventKey="MRI">MRI</MenuItem>
+                    <MenuItem eventKey="DSA">DSA</MenuItem>
+                    <MenuItem eventKey="DR">DR</MenuItem>
+                    <MenuItem eventKey="CR">CR</MenuItem>
+                    <MenuItem eventKey="RF">RF</MenuItem>
+                    <MenuItem eventKey="MG">MG</MenuItem>
+                    <MenuItem eventKey="US">US</MenuItem>
+                  </DropdownButton>
+                  <Button bsStyle="info" style={{ marginLeft: '6px' }} onClick={this.searchCase}>查询</Button>
+                  <Button bsStyle="default" style={{ marginLeft: '3px' }} onClick={() => this.reset()}>重置</Button>
+                </div>
+              </div>
+              <div className="container caseList">
+                <div className="searchResult">{searchResult}</div>
+                <Table striped bordered condensed hover>
+                  <thead>
+                    {tHead}
+                  </thead>
+                  <tbody>
+                    {this.state.cases.length > 0 && this.state.cases.map((specificCase) => {
+                      return (
+                        <tr style={{ backgroundColor: "#F5F5F5" }} key={specificCase._id} onDoubleClick={() => CaseList.jumpTo(specificCase._id)}>
+                          <td>{specificCase.accessionNumber}</td>
+                          <td>{specificCase.patientID}</td>
+                          <td>{specificCase.patientName}</td>
+                          <td>{specificCase.patientAge}</td>
+                          <td>{specificCase.patientSex === 'M' ? '男' : '女'}</td>
+                          <td>{specificCase.studyID}</td>
+                          <td>{specificCase.modality}</td>
+                          <td>{specificCase.studyDate}</td>
+                          <td>{specificCase.patientBirthDate}</td>
+                          <td>{specificCase.studyDescription}</td>
+                          <td>
+                            <Link to={{
+                              pathname: '/viewer',
+                              state: { caseId: specificCase._id }
+                            }} className="glyphicon glyphicon-picture" />
+                            &nbsp;&nbsp;&nbsp;
+                            <Link to={`/newCase?id=${specificCase._id}`}
+                             className="glyphicon glyphicon-pencil" />
+                            &nbsp;&nbsp;&nbsp;
+                            <span className="glyphicon glyphicon-trash" 
+                            onClick={that.deleteCase.bind(this, specificCase._id)} />
+                          </td>
+                        </tr>
+                      )
+                    })
+                    }
+                  </tbody>
+                </Table>
+              </div>
+              <ToastContainer
+                position="bottom-right"
+                type="info"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                pauseOnHover
+              />
+            </div>
+
+          </div>
         </div>
-        <ToastContainer
-          position="bottom-right"
-          type="info"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          pauseOnHover
-        />
       </div>
     )
   }
